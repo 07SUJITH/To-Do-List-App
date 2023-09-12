e5bac116d21abadcc1bbf2b1e6708aeab976c3ba
SUJITH T S <tssujith2002@gmail.com>
2023-09-12 16:12:01 +0530

    added deadline

--- script.js
+++ script.js
@@ -8,31 +8,50 @@ const playAudio = ()=> {
     audio.play();
 }
 
+
 const  addTask = ()=>{
     playAudio();
     if(inputBox.value === ''){
         alert("You must write task..");
     }else{
         let li = document.createElement('li');
-        li.innerHTML = inputBox.value;
+        
+        let completeIcon = document.createElement('span');
+        completeIcon.classList.add("completeIcon");
+        li.appendChild(completeIcon);
+
+        let p =  document.createElement('p');
+        p.classList.add("task");
+
+        let completionTime=  calculateCompletionTime()
+        let task = inputBox.value +"<br>"+ " deadline : "  + completionTime.toString();
+        p.innerHTML= task;
+        li.appendChild(p);
+
+        let removeIcon = document.createElement('span');
+        removeIcon.classList.add("removeIcon");
+        removeIcon.innerHTML ="<i class='fa fa-close red-color'></i>";
+        li.appendChild(removeIcon);
         listContainer.appendChild(li);
-        let span = document.createElement('span');
-        span.innerHTML ="\u00d7";
-        li.appendChild(span);
     }
     inputBox.value = '';
     saveData();
 }
 
+
 const handleItemClick = e =>{
-    if(e.target.tagName ==="LI"){
-        e.target.classList.toggle("checked");
-        saveData();
-    }else if(e.target.tagName ==="SPAN"){
+    if(e.target.classList.contains("removeIcon")){
         e.target.parentElement.remove();
+        saveData();
+        checkAndResetTime();
+    }
+    else if(e.target.classList.contains("completeIcon")){
+        e.target.classList.toggle("checked");
+        e.target.parentElement.classList.toggle("cross-line"); 
         saveData();
     }
 }
+
 
 const handleEnterKeyClick = event =>{
     if(event.key === "Enter"){
@@ -50,10 +69,34 @@ listContainer.addEventListener("click",(e)=>{
     handleItemClick(e);
     audio.play();
 });
-inputBox.addEventListener("keydown", (event)=>{
+const inputField = document.querySelector(".inputField");
+inputField.addEventListener("keydown", (event)=>{
     handleEnterKeyClick(event);
 });
 
+
+let previousCompletionTime = new Date();
+function calculateCompletionTime() {
+    const hoursRequired = parseFloat(document.getElementById("timeRequired").value);
+    if (isNaN(hoursRequired) || hoursRequired <= 0) {
+                alert("Please enter valid task details.");
+                return;
+    }
+    const completionTime = new Date(previousCompletionTime.getTime() + (hoursRequired ) * 60 * 60 * 1000);
+    previousCompletionTime = completionTime;
+    savePrevCompletionTime();
+
+    const options = {
+                hour: 'numeric',
+                minute: 'numeric',
+                hour12: true,
+    };
+
+    const formattedCompletionTime = completionTime.toLocaleTimeString(undefined, options);
+    document.getElementById("timeRequired").value = "";
+    return formattedCompletionTime;
+    
+}
 
 const saveData = ()=>{
     localStorage.setItem("data",listContainer.innerHTML);
@@ -61,6 +104,53 @@ const saveData = ()=>{
 const showTasks = ()=>{
     listContainer.innerHTML = localStorage.getItem("data");
 }
+const savePrevCompletionTime = () => {
+    // Convert the Date object to a string before saving it
+    localStorage.setItem("prevCompletionTime", JSON.stringify(previousCompletionTime));
+}
+const showPrevCompletionTime = () => {
+    // Retrieve the saved string and parse it back to a Date object
+    const savedCompletionTime = localStorage.getItem("prevCompletionTime");
+    if (savedCompletionTime) {
+        previousCompletionTime = new Date(JSON.parse(savedCompletionTime));
+    }
+}
+showPrevCompletionTime();
 showTasks();
 
+
+function checkAndResetTime() {
+    const taskList = document.getElementById("list-container");
+    if (taskList.children.length === 0) {
+        previousCompletionTime = new Date();
+    }
+}
+checkAndResetTime();
+
+
+// ......date and time display............
+function updateDateTime() {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = currentDate.getDate().toString().padStart(2, '0');
+    let hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+    const seconds = currentDate.getSeconds().toString().padStart(2, '0');
+    
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    if (hours > 12) {
+        hours -= 12;
+    }
+    if (hours === 0) {
+        hours = 12;
+    }
+    const formattedDate = `${day}-${month}-${year}`;
+    const formattedTime = `${hours}:${minutes} ${ampm}`;
+    
+    document.getElementById("date").textContent = `${formattedDate}`;
+    document.getElementById("time").textContent = `${formattedTime}`;
+}
+setInterval(updateDateTime, 1000);
+updateDateTime();
 
